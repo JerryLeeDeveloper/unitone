0c622ac49e7f51c6184d8d620e8a00cca51a9471
JerryLi <gimmatek.pc.01@gmail.com>
2019-07-02 15:37:33 +0800

    Finish unit two homework.

--- src/main/kotlin/com/train/TesterKotlin.kt
+++ src/main/kotlin/com/train/TesterKotlin.kt
@@ -3,26 +3,28 @@ package com.train
 import java.util.*
 
 fun main() {
-    val scanner = Scanner(System.`in`)
-    print("Please enter number of tickets: ")
-    val total = scanner.nextInt()
-    print("How many round-trip tickets: ")
-    val round = scanner.nextInt()
-    val ticket = Ticket(total, round, scanner)
-    ticket.calculate()
+    Ticket(Scanner(System.`in`)).continuous()
 }
 
-class Ticket(private var total: Int, private var round: Int, private val scan: Scanner){
+class Ticket(private val scan: Scanner){
     private val singleTrip = 1000
     private val roundTrip = 2000*0.9
+    private val exit = -1
+    private var total = 0
+    private var round = 0
 
-    fun calculate() {
-        if(total < round) {
-            print("Please reset round-trip tickets: ")
-            round = scan.nextInt()
+    fun continuous() {
+        while (total != exit) {
+            if (total == exit) print("Exit")
+            print("Please enter number of tickets: ")
+            total = scan.nextInt()
+            if (total != exit) {
+                print("How many round-trip tickets: ")
+                round = scan.nextInt()
+                val totalPrice = (((total - round) * singleTrip) + ( round * roundTrip)).toInt()
+                println("Total tickets:$total\nRound-trip:$round\nTotal:$totalPrice")
+            }
         }
-        val totalPrice = (((total - round) * singleTrip) + ( round * roundTrip)).toInt()
-        println("Total tickets:$total\nRound-trip:$round\nTotal:$totalPrice")
     }
 
 }
